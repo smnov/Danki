07de43ebbe27169dab2eb09b83b86efdf871fa54
root <root@aleksandr>
2022-12-14 12:36:41 +0800

    api v1

--- backend-api/models/models.py
+++ backend-api/models/models.py
@@ -6,7 +6,9 @@ class Card(SQLModel, table=True):
     id: Optional[int] = Field(primary_key=True)
     frontside: str
     backside: str
-    deck_id: Optional[int] = Field(default=None, foreign_key="deck.id")
+    rating: int = Field(default=10)
+    deck_id: Optional[int] = Field(foreign_key="deck.id")
+    deck_name: str = Field(foreign_key="deck.name")
     deck: Optional["Deck"] = Relationship(back_populates="cards")
 
 class Deck(SQLModel, table=True):
--- backend-api/routers/routers.py
+++ backend-api/routers/routers.py
@@ -1,6 +1,7 @@
 from typing import List
 from models.models import Card, Deck
 from fastapi import APIRouter, status
+from fastapi.exceptions import HTTPException
 from sqlmodel import Session, select
 from db.database import engine
 
@@ -9,20 +10,43 @@ router = APIRouter()
 session = Session(bind=engine)
 
 
-@router.get("/", response_model=List[Card])
+@router.get("/", response_model=List[Card], tags=["cards"])
 async def get_all_cards():
     statement = select(Card)
     res = session.exec(statement).all()
     return res
 
-@router.get("/decks", response_model=List[Deck])
+@router.get("/decks/{id}", response_model=List[Card], tags=["cards"])
+async def get_cards_of_deck(id: int):
+    statement = select(Card).where(Card.deck_id == id)
+    res = session.exec(statement).all()
+    if not res:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+    return res
+
+@router.post("/decks/{id}", response_model=Card, status_code=status.HTTP_201_CREATED, tags=["cards"]) # Create a card
+async def create_card(id: int, card: Card):
+    new_card = Card(frontside=card.frontside, backside=card.backside, deck_id=id)
+    session.add(new_card)
+    session.commit()
+    return new_card
+
+@router.get("/decks", response_model=List[Deck], tags=["decks"])
 async def get_decks():
     statement = select(Deck)
     res = session.exec(statement).all()
     return res
 
+# @router.get("/decks/{id}", response_model=Deck, tags=["decks"])
+# async def get_deck_by_id(id: int):
+#     statement = select(Deck).where(Deck.id == id)
+#     res = session.exec(statement).first()
 
-@router.post("/decks", response_model=Deck, status_code=status.HTTP_201_CREATED)
+#     if not res:
+#         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+
+#     return res
+@router.post("/decks", response_model=Deck, status_code=status.HTTP_201_CREATED, tags=["decks"])
 async def create_deck(deck: Deck):
     new_deck = Deck(name=deck.name)
     session.add(new_deck)
